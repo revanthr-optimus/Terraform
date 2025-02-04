6a0708e971c8d0c9e448677b8fe53d2f54c66608
revanthr-optimus <89133295+revanthr-optimus@users.noreply.github.com>
2025-02-04 12:31:09 +0530

    Update locals.tf

--- Terraform_Project/terraform/locals.tf
+++ Terraform_Project/terraform/locals.tf
@@ -1,19 +1,4 @@
 locals {
   resource_location="North Europe"
   resource_group_name = "IaCLearning-RG"
-  virtual_network={
-    name="app-network"
-    address_prefixes=["10.0.0.0/16"]
-  }
-  subnet_address_prefix=["10.0.0.0/24","10.0.1.0/24"]
-  subnets=[
-    {
-        name="websubnet01"
-        address_prefixes=["10.0.0.0/24"]
-    },
-    {
-        name="appsubnet01"
-        address_prefixes=["10.0.1.0/24"]
-    }
-  ]
 }
