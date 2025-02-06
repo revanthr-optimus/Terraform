3095e40452dbbb8c941aa44aa1ac58be2941c844
revanthr-optimus <89133295+revanthr-optimus@users.noreply.github.com>
2025-02-06 15:43:06 +0530

    Delete Terraform_Project directory

--- Terraform_Project/terraform/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  resource_location="North Europe"
-  resource_group_name = "IaCLearning-RG"
-}
--- Terraform_Project/terraform/modules/azurebus/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "servicebus_config" {
-    type = object({
-        namespace_name       = string
-        location             = string
-        resource_group_name  = string
-        sku                  = string
-        queue_name           = string
-    })
-}
